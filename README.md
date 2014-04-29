4e0e1d8ffe81278c6b949591199ca89556f68913
peachananr <peachananr@gmail.com>
2014-04-29 21:05:41 +0700

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-parallax-scroller
-=================
+#Parallax Scroller by Pete R.
+A jQuery plugin that let you scroll through lists with a smooth parallax effect
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [Travelistly](http://www.travelistly.com) and [BucketListly](http://www.bucketlistly.com)
 
-Scroll through lists with a smooth parallax effects
+License: [Attribution-ShareAlike 4.0 International](http://creativecommons.org/licenses/by-sa/4.0/deed.en_US)
+
+[![Parallax Scroller](http://www.thepetedesign.com/images/parallax-scroller_image.png "Parallax Scroller")](http://www.thepetedesign.com/demos/parallax-scroller_demo.html)
+
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/parallax-scroller_demo.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on both desktop and smartphones have been tested. Not tested on IE.
+
+## Basic Usage
+To use this plugin, simply include the latest jQuery library (preferably version 2.0.0 or higher) together with `jquery.parallax-scroll.js` and `parallax-scroll.css` into your document's `<head>` follow by an HTML markup as follows:
+
+````html
+<body>
+  ..
+  <div class="list">
+    <div class="list-item" data-ps-bg="images/1.jpg">
+      ...
+    </div>
+    <div class="list-item" data-ps-bg="images/2.jpg">
+      ...
+    </div>
+  ..
+</body>
+
+````
+
+The `data-ps-bg` attribute must reflect the location of the image you want to use as the parallax background of that item. For example, if your image is in the images folder and is called "1.jpg", change the attribute to "images/1.jpg".
+
+Once you got your list setup, call the function as follows:
+
+Note: All the elements directly under the selector you called the script on, will be considered as a parallax item.
+
+````javascript
+ $(".list").parallaxScroll({
+   parallaxSpeed: 3,            // You can set the speed of the parallax when scroll. Values can goes from 1 - 10. The default value is 3
+   tilt: false,                 // You can define how much you want the list to tilt. Values can be negative (tilt to the left) or positive (tilt to the right). For example: -1, -2, -3 or 1, 2, 3. The default value is false.
+   parallax: true,              // You can choose NOT to make the list parallax by toggling this to true, but who would do that?
+   startPosition: "0%",         // Since the plugin is using background CSS style, you can define the initial point of the background so that it is centered the way you like. This option on excepts percentage. The default value is "0%"
+   retainNativeScroll: true     // This option will let you override native scroll, and add the effect onto a scrollable container instead of the whole page. Simply toggle this to false, and div container "list" will be scrollable. The default value is true.
+ });
+````
+
+By default, the plugin uses the native scrolling mechanism of the whole page to reflect the parallax effect onto items' background that come into view. If you want to make a container scrollable and also retain this parallax effect, simply toggle `retainNativeScroll` to false and the plugin will do it all for you.
+
+Now you will have a beautiful list with individual parallax effect with almost no hassle.
+
+If you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#plugins), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- Tutorial (Coming Soon)
---
+++ demo/jquery.parallax-scroller.js
@@ -0,0 +1,171 @@
+/* ===========================================================
+ * jquery.parallax-scroller.js v1.0
+ * ===========================================================
+ * Copyright 2014 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Scroll through lists with a smooth
+ * parallax effect
+ *
+ * https://github.com/peachananr/parallax-scroller
+ * 
+ * License: GPL v3
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    parallaxSpeed: 3,
+    tilt: false,
+    parallax: true,
+    startPosition: "0%",
+    retainNativeScroll: true
+  };  
+  
+  $.fn.parallaxScroller = function(options){
+    return this.each(function(){
+      var settings = $.extend({}, defaults, options),
+          el = $(this),
+          originY = 0;
+      
+      // Wrap all list items in a scroller to be used to scroll    
+      el.addClass("ps-list").find(" > *").wrapAll("<div class='ps-scroller'></div>")
+      
+      
+      // Define background according to the markup
+      el.find(" > .ps-scroller > *").each(function(){
+        var el2 = $(this);
+        el2.addClass("ps-list-item").css({
+          background: "url(" + el2.data("ps-bg") + ") center center no-repeat",
+          "background-size": "100% auto",
+          "background-position": "50% " + settings.startPosition
+        })
+        // If tilt is enable, apply here
+        if (settings.tilt != false) {
+          el2.css({
+            "-webkit-transform": "matrix(1," + (settings.tilt * 0.01) + ",0,1,0,0)",
+            "-moz-transform": "matrix(1," + (settings.tilt * 0.01) + ",0,1,0,0)",
+            "-o-transform": "matrix(1," + (settings.tilt * 0.01) + ",0,1,0,0)",
+            "transform": "matrix(1," + (settings.tilt * 0.01) + ",0,1,0,0)"
+          })
+          
+        }
+        
+      });
+      
+      if (settings.retainNativeScroll == true) {
+        el.addClass("ps-native")
+        el = $("body");
+        
+      }
+      
+      // If Parallax is enable, run this
+      if (settings.parallax == true) {
+        el.find('.ps-list-item').each(function(){
+          var el3 = $(this);
+          
+          // bind on scroll to create parallax effect on the background 
+          el.on('mousewheel DOMMouseScroll', function(e) {
+
+            if (settings.retainNativeScroll == true) {
+              originY = $(document).scrollTop();
+            }
+            
+            // Parallax startes/stops only when object is on screen
+            if (el3.is_on_screen()) {
+              
+              if (settings.retainNativeScroll == true) {
+                var wh = $(document).height();
+              } else {
+                var wh = el.find(".ps-scroller").height()
+              }      
+              
+              if (originY < wh  - el.height() && originY > 0 ) {
+                var backgroundPos = el3.css('backgroundPosition').split(" "),
+                    y = originY/(wh - el.height()) * 100,
+                    delta = -e.originalEvent.detail || e.originalEvent.wheelDelta;
+                  // Scrolling Up   
+                 if (delta < 0) {
+                   y = Math.min(Math.max(parseFloat(backgroundPos[1]) + (settings.parallaxSpeed/5), parseFloat(settings.startPosition)), 100);
+                 } else {
+                   // Scrolling Down
+                   y = Math.min(Math.max(parseFloat(backgroundPos[1]) - (settings.parallaxSpeed/5), parseFloat(settings.startPosition)), 100);
+                   
+                    
+                 }           
+                   
+                 // Set the new Y Coord for background
+                 var coords = '50% '+ y + '%';
+                 el3.css({ "background-position": coords });
+                 
+              }
+              
+               
+            }
+
+          });
+        });
+      }
+      
+      
+      
+      
+      // Bind a scrolling event that will let users scroll down the container
+      if (settings.retainNativeScroll != true) {
+        el.on('mousewheel DOMMouseScroll', function(e, delta) {
+          var $scrollTop = originY;
+
+
+          delta = delta || -e.originalEvent.detail / 3 || e.originalEvent.wheelDelta / 120;
+
+
+          $scrollTop = Math.min(el.find(".ps-scroller").height()  - el.height(), Math.max(0, parseInt($scrollTop - delta * 30)));
+          el.find(".ps-scroller").stop().animate({
+              top: -1 * $scrollTop + 'px'
+          }, 0, 'linear');
+
+
+          originY = $scrollTop;
+
+
+
+          e.stopPropagation();
+          return false;
+        });
+      } 
+      
+      
+      // Function Check if the when the element appears on the screen
+      
+      $.fn.is_on_screen = function(){
+          var win = el;
+          var viewport = {
+              top : originY
+          };
+          
+          
+          
+          viewport.bottom = viewport.top + win.height();
+    
+          var bounds = this.offset();
+          
+          if (settings.retainNativeScroll == true) {
+            bounds.top = this.offset().top;
+            bounds.bottom = this.offset().top + this.outerHeight();
+          } else {
+            bounds.top = (this.offset().top - el.find(".ps-scroller").offset().top);
+            bounds.bottom = (this.offset().top - el.find(".ps-scroller").offset().top + this.outerHeight()) + this.outerHeight();
+          }
+          
+          
+          return (!(viewport.bottom < bounds.top || viewport.top > (bounds.bottom - 100)));
+      };
+      
+      
+    });
+    
+  }
+  
+
+}(window.jQuery);
